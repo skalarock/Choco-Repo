fd4206f82580ef6df2c5ca973e132c127908fd81
Dominik <40644137+skalarock@users.noreply.github.com>
2019-02-14 14:36:39 +0100

    Cocholoatey pack installation via powershell with simpler script

---
+++ choco_pack_install_simple.ps1
@@ -0,0 +1,1 @@
+choco install  googlechrome git GitHub notepadplusplus.install 7zip.install firefox virtualbox vscode sql-server-management-studio rdmfree thunderbird vlc speedfan skype libreoffice-fresh putty python3 keepass gimp -y
