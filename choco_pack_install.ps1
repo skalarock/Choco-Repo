eed037c99a57c93106f33322c54648386d235dc3
Dominik <40644137+skalarock@users.noreply.github.com>
2020-02-12 10:52:03 +0100

    Adding and removing apps

--- choco_pack_install.ps1
+++ choco_pack_install.ps1
@@ -1,4 +1,4 @@
-$Packages = '7zip.install', 'autohotkey.portable', 'ccleaner', 'firefox', 'git', 'github-desktop', 'googlechrome', 'hugo', 'libreoffice-fresh', 'megasync', 'notepadplusplus.install', 'putty', 'python3', 'rdmfree', 'skype', 'speedfan', 'sql-server-management-studio', 'thunderbird', 'virtualbox', 'vlc', 'vscode', 'youtube-dl'
+$Packages = '7zip.install', 'audacity', 'autohotkey.portable', 'curl', 'ffmpeg', 'firefox', 'git', 'github-desktop', 'googlechrome', 'hugo', 'libreoffice-fresh', 'megasync', 'notepadplusplus.install', 'putty', 'python3', 'rdmfree', 'skype', 'speedfan', 'sql-server-management-studio', 'telegram.portable', 'thunderbird', 'virtualbox', 'vim', 'vlc', 'vscode', 'youtube-dl'
 
 ForEach ($PackageName in $Packages)
 {
