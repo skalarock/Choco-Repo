df2c53ce8c33f6d00c3ff59240763151a1917b92
Dominik <40644137+skalarock@users.noreply.github.com>
2020-03-20 19:37:55 +0100

    Update choco_pack_install.ps1

--- choco_pack_install.ps1
+++ choco_pack_install.ps1
@@ -1,4 +1,4 @@
-$Packages = '7zip.install', 'audacity', 'autohotkey.portable', 'curl', 'ffmpeg', 'firefox', 'git', 'github-desktop', 'googlechrome', 'hugo', 'libreoffice-fresh', 'megasync', 'notepadplusplus.install', 'putty', 'python3', 'rdmfree', 'skype', 'speedfan', 'sql-server-management-studio', 'telegram.portable', 'thunderbird', 'virtualbox', 'vim', 'vlc', 'vscode', 'youtube-dl'
+$Packages = '7zip.install', 'audacity', 'autohotkey.portable', 'curl', 'ffmpeg', 'firefox', 'git', 'github-desktop', 'googlechrome', 'hugo', 'libreoffice-fresh', 'megasync', 'notepadplusplus.install', 'putty', 'python3', 'rdmfree', 'skype', 'speedfan', 'joplin', 'shotcut', 'telegram.portable', 'thunderbird', 'virtualbox', 'vim', 'vlc', 'vscode', 'youtube-dl'
 
 ForEach ($PackageName in $Packages)
 {
