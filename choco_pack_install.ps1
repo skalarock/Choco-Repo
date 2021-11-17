9264ffd8723c72dbcd5009d1d276e7b36e4a5ce9
Dominik <40644137+skalarock@users.noreply.github.com>
2021-11-17 10:04:28 +0100

    Update choco_pack_install.ps1
    removing and adding packages

--- choco_pack_install.ps1
+++ choco_pack_install.ps1
@@ -1,4 +1,4 @@
-$Packages = '7zip.install', 'audacity', 'autohotkey.portable', 'curl', 'ffmpeg', 'firefox', 'git', 'googlechrome', 'hugo', 'libreoffice-fresh', 'megasync', 'notepadplusplus.install', 'putty', 'python3', 'rdmfree', 'skype', 'speedfan', 'joplin', 'shotcut', 'telegram.portable', 'thunderbird', 'virtualbox', 'vlc', 'vscode', 'youtube-dl'
+$Packages = '7zip.install', 'audacity', 'autohotkey.portable', 'curl', 'ffmpeg', 'firefox', 'git', 'googlechrome', 'hugo', 'libreoffice-fresh', 'megasync', 'notepadplusplus.install', 'putty', 'python3', 'rdmfree', 'skype', 'speedfan', 'joplin', 'shotcut', 'telegram.portable', 'thunderbird', 'discord', 'vlc', 'vscode', 'youtube-dl'
 
 ForEach ($PackageName in $Packages)
 {
